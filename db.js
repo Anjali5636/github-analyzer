8a56c93490034da8dc09247682ff54f5b1790e2d
Anjali5636 <ontipulianjali@gmail.com>
2026-06-06 23:00:06 +0530

    updated db connection with env variables

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "github_db"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("DB connection failed", err);
+    console.log("DB Connection Failed");
+    console.log(err);
   } else {
-    console.log("MySQL Connected Successfully");
+    console.log("Railway MySQL Connected Successfully");
   }
 });
 
